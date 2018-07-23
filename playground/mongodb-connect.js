e5d9ad4dbae51d9a9630f42db027a70a98bc4e1c
Bharat Jain <bharatjain7723@gmail.com>
2018-07-23 12:31:19 +0530

    Wrote data in MongoDB. Code according to mongodb -v greater than 3.0.0

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,38 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+//Connects to the url, whether of AWS or something else
+MongoClient.connect('mongodb://localhost:27017',{ useNewUrlParser: true }, (err, client)=>{
+    if(err){
+        return console.log("Unable to connect to mongodb server");
+    }
+    console.log("Connected to mongodb server");
+    const dbName = 'TodoApp'
+
+    const db = client.db(dbName);
+    db.collection('Todos').insertOne({
+        text: 'something to do',
+        completed: false
+    }, (err, result) =>{
+        if(err){
+            return console.log("Unable to insert todo", err);          
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
+    db.collection('Users').insertOne({
+        name: 'Bharat',
+        age: 22,
+        location: 'Delhi' 
+    }, (err, result)=>{
+        if(err){
+            return console.log("Unable to insert Users", err);
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
+    client.close();
+});
